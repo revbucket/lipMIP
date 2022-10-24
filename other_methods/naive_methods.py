98413719f5e5cdd80e8c1d6a08903ec847556043
revbucket <mattjordan.mail@gmail.com>
2022-10-24 10:41:39 -0700

    Cleaned up support for ZLip

--- other_methods/naive_methods.py
+++ other_methods/naive_methods.py
@@ -1,11 +1,11 @@
-""" Naive methods for Lipschitz computation 
+""" Naive methods for Lipschitz computation
 	For the L2 domain:
 	Upper bound = Prod(||W_1||, ||W_2||, ..., ||W_l||)
 	Lower bound = ||Prod(W_1, W_2, ..., W_l)||
 
 	Need some work to show for L-inf/L-1
 """
-import numpy as np 
+import numpy as np
 from .other_methods import OtherResult
 import utilities as utils
 
@@ -30,7 +30,7 @@ class NaiveUB(OtherResult):
 		# BE CAREFUL HERE -- IS THIS THE BEST WE CAN DO????
 		return np.linalg.norm(utils.as_numpy(w), np.inf) # IS THIS RIGHT???
 
-	@classmethod 
+	@classmethod
 	def l1_norm(cls, w):
 		return np.linalg.norm(utils.as_numpy(w), 1)
 
@@ -76,26 +76,26 @@ class RandomLB(OtherResult):
 	""" Randomly select points and return the max gradient"""
 	def __init__(self, network, c_vector, domain, primal_norm):
 		super(RandomLB, self).__init__(network, c_vector, domain, primal_norm)
-		self.max_norm = None 
-		self.max_point = None 
+		self.max_norm = None
+		self.max_point = None
 		self.max_grad = None
 		self.compute_time = None
 	def compute(self, num_points=1000):
-		""" Computes maximum of dual norm of gradients of random points. 
+		""" Computes maximum of dual norm of gradients of random points.
 			Can be called multiple times and will only improve
 		"""
 		timer = utils.Timer()
 		dual = {'l1':  np.inf, 'l2': 2, 'linf': 1}[self.primal_norm]
 
-		random_output = self.network.random_max_grad(self.domain, self.c_vector, 
+		random_output = self.network.random_max_grad(self.domain, self.c_vector,
 													 num_points, pnorm=dual)
 		if (self.max_norm is None) or (random_output['norm'] > self.max_norm):
 			self.max_norm  = random_output['norm']
 			self.max_point = random_output['point']
 			self.max_grad  = random_output['grad']
 
-		self.value = self.max_norm # redundancy here 
+		self.value = self.max_norm # redundancy here
 		if self.compute_time is None:
 			self.compute_time = 0
 		self.compute_time += timer.stop()
-		return self.value
+		return self.value.item()
--- other_methods/z_lip.py
+++ other_methods/z_lip.py
@@ -3,11 +3,11 @@
 
 import numpy as np
 import math
-from .other_methods import OtherResult 
-import utilities as utils 
+from .other_methods import OtherResult
+import utilities as utils
 from pre_activation_bounds import PreactivationBounds
 from interval_analysis import AbstractNN
-import bound_prop as bp 
+import bound_prop as bp
 
 
 class ZLip(OtherResult):
@@ -17,16 +17,18 @@ class ZLip(OtherResult):
 	def compute(self):
 		# Fast lip is just interval bound propagation through backprop
 		timer = utils.Timer()
-		ap = bp.AbstractParams.basic_zono() 
-		ann = bp.AbstractNN2(self.network) 
+		ap = bp.AbstractParams.basic_zono()
+		ann = bp.AbstractNN2(self.network)
 
 		self.grad_range = ann.get_both_bounds(ap, self.domain, self.c_vector)[1].output_range
 
-		if self.primal_norm == 'linf': 
-			value = self.grad_range.maximize_l1_norm_abs() 
+		if self.primal_norm == 'linf':
+			value = self.grad_range.maximize_l1_norm_abs()
 		else:
-			value = torch.max(self.grad_range.lbs.abs(), 
-							  self.grad_range.ubs.abs()).max() 
+			value = torch.max(self.grad_range.lbs.abs(),
+							  self.grad_range.ubs.abs()).max()
+
+		value = value.item()
 		self.value = value
 		self.compute_time = timer.stop()
 		return value
